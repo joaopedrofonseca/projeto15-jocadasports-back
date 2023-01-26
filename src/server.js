659dbc7962643da67d4d21e3865bad587060e84d
Dani Rosa <danirosa.dev@gmail.com>
2023-01-26 20:50:58 -0300

    feat: produtos

--- src/server.js
+++ src/server.js
@@ -2,13 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import authRouter from './routes/authRouter.js'
+import productsRouter from './routes/productsRouter.js'
 
 dotenv.config()
 const server = express()
 server.use(cors())
 server.use(express.json())
 
-server.use([authRouter])
+server.use([authRouter, productsRouter])
 
 
 server.listen(process.env.PORT, () => {
